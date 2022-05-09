7daf6fb3ab4fff36e1ca4b6613b8299892f06fac
Christian Caicedo <christian.caicedo@grainger.com>
2022-05-09 11:39:35 -0500

    se separa cheques vencidos pa cobrar de los que estan por cobrar

--- src/app/components/por-cobrar/por-cobrar.component.ts
+++ src/app/components/por-cobrar/por-cobrar.component.ts
@@ -9,35 +9,41 @@ import { CheckModel, CheckStatus } from 'src/app/models/check.model';
   styleUrls: ['./por-cobrar.component.scss']
 })
 export class PorCobrarComponent implements OnInit {
-  checks:CheckModel[]=[]
-  constructor(public dialog: MatDialog) {}
+  checks: CheckModel[] = []
+  pendings: CheckModel[] = []
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
-    const checks=localStorage.getItem(CheckStatus.PENDING)
-    if(checks){
-      this.checks=JSON.parse(checks);
+    const checks = localStorage.getItem(CheckStatus.PENDING)
+    const today = new Date();
+    today.setHours(23, 59, 59)
+    if (checks) {
+      const stored: CheckModel[] = JSON.parse(checks);
+      this.pendings = stored.filter(c => (new Date (c.date)).getTime() <= today.getTime());
+      this.checks=stored.filter(c=>!this.pendings.find(p=>c.id==p.id))
     }
   }
 
   removeCheck(ck: CheckModel) {
     const dialogRef = this.dialog.open(ConfirmComponent, {
       width: '250px',
-      data: {message:'Desea dar este cheque por cobrado?'},
+      data: { message: 'Desea dar este cheque por cobrado?' },
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result){
+      if (result) {
         this.checks = this.checks.filter(check => check.id !== ck.id);
-        ck.status=CheckStatus.PAYED;
-        ck.datePayed=new Date();
-        localStorage.setItem(CheckStatus.PENDING,JSON.stringify(this.checks));
-        const history=localStorage.getItem(CheckStatus.PAYED);
-        if(history){
-          const historyToSave=JSON.parse(history)
+        ck.status = CheckStatus.PAYED;
+        ck.datePayed = new Date();
+        localStorage.setItem(CheckStatus.PENDING, JSON.stringify(this.checks));
+        const history = localStorage.getItem(CheckStatus.PAYED);
+        if (history) {
+          const historyToSave = JSON.parse(history)
           historyToSave.unshift(ck);
-          localStorage.setItem(CheckStatus.PAYED,JSON.stringify(historyToSave));
-        }else{
-          localStorage.setItem(CheckStatus.PAYED,JSON.stringify([ck]));
+          localStorage.setItem(CheckStatus.PAYED, JSON.stringify(historyToSave));
+        } else {
+          localStorage.setItem(CheckStatus.PAYED, JSON.stringify([ck]));
 
         }
       }
